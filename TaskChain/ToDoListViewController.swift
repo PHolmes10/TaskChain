3b0fecce3a1be9f736dbbf7b9a44359ed78491e5
pholmes10 <pholmes10@gmail.com>
2019-05-30 18:31:45 -0600

    Functionality for adding a new task to the exsiting list

--- TaskChain/ToDoListViewController.swift
+++ TaskChain/ToDoListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ToDoListViewController: UITableViewController {
 
-    let itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+    var itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -48,6 +48,34 @@ class ToDoListViewController: UITableViewController {
 
         
         tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    //MARK - Add New Tasks
+    
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Task", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Task", style: .default) { (action) in
+            //what will happen when user clicks 'Add Task'
+            
+            self.itemArray.append(textField.text!)
+            
+            self.tableView.reloadData()
+            
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "New Task"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
     }
 }
 
